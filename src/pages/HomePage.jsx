cb6adf4cd8dca56c7ff77436e71489d22c797285
Nitin Dixit <nitinajaydixit@gmail.com>
2025-05-04 06:34:13 +0530

    Add:HompePage, NotFoundPage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,39 @@
+import { useCallback, useState } from 'react';
+
+import DataRenderer from '@/components/DataRenderer';
+import ListingFilters from '@/components/ListingFilters';
+import ListingList from '@/components/ListingList';
+import { Separator } from '@/components/ui';
+import useListingsQuery from '@/hooks/queries/useListingsQuery';
+
+const HomePage = () => {
+  const [filters, setFilters] = useState({
+    dates: undefined,
+    guests: 0,
+    search: '',
+  });
+
+  const {
+    data: { data: listings } = {},
+    error,
+    isLoading,
+  } = useListingsQuery(filters);
+
+  const handleFilters = useCallback((filters) => {
+    setFilters(filters);
+  }, []);
+
+  return (
+    <div className='px-8 py-4' data-testid='home-page'>
+      <div className='mb-4'>
+        <ListingFilters onChange={handleFilters} />
+        <Separator className='my-4' />
+      </div>
+      <DataRenderer error={error} isLoading={isLoading}>
+        <ListingList listings={listings} />
+      </DataRenderer>
+    </div>
+  );
+};
+
+export default HomePage;
