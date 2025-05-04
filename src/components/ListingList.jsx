bbfcb66dd11422b5e7d61d16f221125335b33629
Nitin Dixit <nitinajaydixit@gmail.com>
2025-05-04 11:13:53 +0530

    Add: components DataRenderer, ListingFilters, ListingList

---
+++ src/components/ListingList.jsx
@@ -0,0 +1,20 @@
+import ListingCard from '@/components/ListingCard';
+
+const ListingList = ({ listings }) => {
+  return (
+    <div
+      className='flex flex-wrap justify-center gap-4'
+      data-testid='listing-list'
+    >
+      {listings.length > 0 ? (
+        listings.map((listing) => (
+          <ListingCard key={listing.id} listing={listing} />
+        ))
+      ) : (
+        <p>No listings found.</p>
+      )}
+    </div>
+  );
+};
+
+export default ListingList;
