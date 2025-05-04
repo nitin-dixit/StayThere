5938094716fa90094a53fcd53cf4cb44aed822fa
Nitin Dixit <nitinajaydixit@gmail.com>
2025-05-04 07:11:03 +0530

    Add:Router.jsx

---
+++ src/Router.jsx
@@ -0,0 +1,74 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Route from '@/components/Route';
+import PostListingPage from '@/pages/PostListingPage';
+import HomePage from '@/pages/HomePage';
+import ListingDetailsPage from '@/pages/ListingDetailsPage';
+import ListingFavoritesPage from '@/pages/ListingFavoritesPage';
+import NotFoundPage from '@/pages/NotFoundPage';
+import ProfilePage from '@/pages/ProfilePage';
+import SignInPage from '@/pages/SignInPage';
+
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        path: '/signin',
+        element: (
+          <Route>
+            <SignInPage />
+          </Route>
+        ),
+      },
+      {
+        path: '/',
+        element: (
+          <Route isProtected>
+            <HomePage />
+          </Route>
+        ),
+      },
+      {
+        path: '/listings/post',
+        element: (
+          <Route isProtected>
+            <PostListingPage />
+          </Route>
+        ),
+      },
+      {
+        path: '/listings/:listingId',
+        element: (
+          <Route isProtected>
+            <ListingDetailsPage />
+          </Route>
+        ),
+      },
+      {
+        path: '/favorites',
+        element: (
+          <Route isProtected>
+            <ListingFavoritesPage />
+          </Route>
+        ),
+      },
+      {
+        path: '/profile',
+        element: (
+          <Route isProtected>
+            <ProfilePage />
+          </Route>
+        ),
+      },
+    ],
+  },
+]);
+
+const Router = () => <RouterProvider router={router} />;
+
+export default Router;
